f4e3edb69d30ceb313a6964a3cf727bbabd5f4e6
david <davidtucu@hotmail.com>
2022-09-30 09:04:26 -0300

    configracion del entorno Layout Prisma modelos y mysql seeding

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
 import { PrismaClient } from '@prisma/client'
 import Head from 'next/head'
 import Image from 'next/image'
-
-export default function Home({categorias}) {
-  console.log(categorias)
+import Layout from '../layout/Layout.js'
+export default function Home() {
+  // console.log(categorias)
 
   return (
-   <h1>Next j1 </h1>
+    <>
+    <h1>Next j1 </h1>
+    <Layout pagina="Menu">
+      
+    </Layout>
+    </>
+    
   )
 }
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient()
-  const categorias = await prisma.categoria.findMany()
-  return {
-    props: {
-      categorias,
-    }
-  }
-}
+// export const getServerSideProps = async () => {
+//   const prisma = new PrismaClient()
+//   const categorias = await prisma.categoria.findMany()
+//   return {
+//     props: {
+//       categorias,
+//     }
+//   }
+// }
